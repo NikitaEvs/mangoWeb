82ee9ec9b33f2f2fb907068cc91eccb6a108c566
NikitaEvs <nikitaevsykov2@gmail.com>
2020-05-05 11:20:07 +0300

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,20 @@ FROM python:3.7
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+ENV DJANGO_DB_HOST="db" \
+    DJANGO_DB_PORT="5432" \
+    DJANGO_SUPERUSER_NAME="cat" \
+    DJANGO_SUPERUSER_MAIL="cat@mail" \
+    DJANGO_SUPERUSER_PASS="meow"
+
+RUN mkdir mango
 WORKDIR /mango
 
 COPY requirements.txt /mango/
 RUN pip3 install -r requirements.txt
 
 COPY . /mango/
+RUN chmod +x /mango/docker-entrypoint.sh
+
+ENTRYPOINT ["/mango/docker-entrypoint.sh"]
+CMD ["runserver", "0.0.0.0:8000"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-echo "Collect static files"
-python manage.py collectstatic --noinput
+create_superuser="
+import django
+django.setup()
+from django.contrib.auth.models import User
+try:
+    User.objects.create_superuser('$DJANGO_SUPERUSER_NAME', '$DJANGO_SUPERUSER_MAIL', '$DJANGO_SUPERUSER_PASS')
+except Exception:
+    pass
+"
 
-echo "Apply database migrations"
-python manage.py migrate
+create_superuser() {
+    if [ -z "$DJANGO_SUPERUSER_NAME" ] || [ -z "$DJANGO_SUPERUSER_MAIL" ] || [ -z "$DJANGO_SUPERUSER_PASS" ]; then
+        echo "Environment variables for database not set, not creating superuser."
+    else
+        echo "Creating superuser"
+        python -c "$create_superuser"
+    fi
+}
 
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+wait_for_db() {
+    if [ -z "$DJANGO_DB_HOST" ] || [ -z "$DJANGO_DB_PORT" ]; then
+        echo "No django database host or port, not waiting for db."
+    else
+        echo "Waiting for database"
+        dockerize -wait tcp://"$DJANGO_DB_HOST":"$DJANGO_DB_PORT" -timeout 30s
+    fi
+}
+
+if [ "$1" == "runserver" ]; then
+    wait_for_db
+
+    echo "Running migrations"
+    python manage.py migrate
+
+    echo "Running collectstatic"
+    python manage.py collectstatic --noinput
+
+    create_superuser
+
+    exec python manage.py "$@"
+fi
