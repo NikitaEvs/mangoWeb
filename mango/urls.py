cd76b33637904a99ba6adb9204899b2974820dfe
NikitaEvs <nikitaevsykov2@gmail.com>
2020-05-07 21:29:48 +0300

    Add url to the main engine

--- mango/urls.py
+++ mango/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from mango import views
-from django.contrib.auth.views import LoginView
+from mango.views import (DayListView, AccountView, TasksView)
 
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('mango/', views.engine, name='mango')
+    path('day/', DayListView.as_view(), name='day'),
+    path('add/', views.day_add, name='add'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('tasks/', TasksView.as_view(), name='tasks'),
+    path('account/', AccountView.as_view(), name='account')
 ]
