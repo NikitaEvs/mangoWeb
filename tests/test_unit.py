3bed3d9c6f57f5c3d40cafec2520eca1b0b1269b
NikitaEvs <nikitaevsykov2@gmail.com>
2020-05-08 21:03:30 +0300

    Fix typo in test_unit

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from mango.models import Task
 from mango.views import get_running_task
@@ -11,8 +12,12 @@ class EngineUnit(TestCase):
     def setUpTestData(cls):
         user = User.objects.create_user(username="cat", password="meow")
         user.save()
-        task = Task.objects.create(user=user, task_name="do", task_priority=1)
-        task.is_running = True
+        task = Task.objects.create(user=user,
+                                   task_name="do",
+                                   task_priority=1,
+                                   date_start=timezone.now(),
+                                   is_running=True)
+
         another_task = Task.objects.create(user=user,
                                            task_name="something",
                                            task_priority=2)
@@ -20,9 +25,10 @@ class EngineUnit(TestCase):
         task.save()
         another_task.save()
 
-    def get_running(self):
+    def test_get_running(self):
         self.client.login(username="cat", password="meow")
         request = self.client.get("/day/")
+        request.user = User.objects.get(username="cat")
 
         task = get_running_task(request)
         self.assertEquals(task.task_name, "do")
